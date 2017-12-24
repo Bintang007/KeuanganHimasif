884a280b69ca1b7e2d541bd00640d836aa91ef75
Brian R <rizqibrian27@gmail.com>
2017-12-24 13:06:31 +0700

    Tambah datepicker

--- app/src/main/java/com/example/brianr/keuanganhimasif/TambahDanaKeluar.java
+++ app/src/main/java/com/example/brianr/keuanganhimasif/TambahDanaKeluar.java
@@ -1,13 +1,58 @@
 package com.example.brianr.keuanganhimasif;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class TambahDanaKeluar extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+public class TambahDanaKeluar extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    TextView title;
+    Typeface tf1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_dana_keluar);
+        title = (TextView) findViewById(R.id.title);
+        tf1 = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+        title.setTypeface(tf1);
+    }
+
+    public void datePicker(View view) {
+        TambahDanaMasuk.DatePickerFragment fragment = new TambahDanaMasuk.DatePickerFragment();
+        fragment.show(getFragmentManager(), "date");
+
+    }
+
+    private void setDate(final Calendar calendar) {
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        ((TextView) findViewById(R.id.showdate)).setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal = new GregorianCalendar(year, month, day);
+        setDate(cal);
+    }
+
+    public static class DatePickerFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)
+                            getActivity(), year, month, day);
+        }
     }
 }
--- app/src/main/java/com/example/brianr/keuanganhimasif/TambahDanaMasuk.java
+++ app/src/main/java/com/example/brianr/keuanganhimasif/TambahDanaMasuk.java
@@ -3,6 +3,7 @@ package com.example.brianr.keuanganhimasif;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,46 +11,54 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class TambahDanaMasuk extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
-
+    Typeface tf1;
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_dana_masuk);
+        title = (TextView) findViewById(R.id.title);
+        tf1 = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+        title.setTypeface(tf1);
     }
 
     public void datePicker(View view) {
         DatePickerFragment fragment = new DatePickerFragment();
-        fragment.show(getSupportFragmentManager(),"date");
+        fragment.show(getFragmentManager(),"date");
+
     }
 
-    private void setDate(final Calendar calendar){
+    private void setDate(final Calendar calendar) {
         final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
         ((TextView) findViewById(R.id.showdate)).setText(dateFormat.format(calendar.getTime()));
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        Calendar cal = new GregorianCalendar(year,month,day);
+        Calendar cal = new GregorianCalendar(year, month, day);
         setDate(cal);
     }
 
     public static class DatePickerFragment extends DialogFragment {
-
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             final Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
-            return super.onCreateDialog(savedInstanceState);
-        }
-
-        public void show(FragmentManager supportFragmentManager, String date) {
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)
+                        getActivity(),year,month,day);
         }
     }
+
+
 }
